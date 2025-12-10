2f13f351fd4a74cb2a03ac32171d030838e24493
hawkins1704 <renzoarroyo09@gmail.com>
2025-12-10 15:44:48 -0500

    Node type added to compilerOptions

--- src/pages/admin/AdminCategoryDetail.tsx
+++ src/pages/admin/AdminCategoryDetail.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect,  useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { useMutation, useQuery } from "convex/react";
 import type { ChangeEvent, FormEvent } from "react";
