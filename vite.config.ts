e2f766531587e7ac6c6c9078093017794e3da208
hawkins1704 <168672582+renzo1704@users.noreply.github.com>
2026-02-02 20:45:15 -0500

    Prerendering landing page

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { vitePrerenderPlugin } from 'vite-prerender-plugin'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    vitePrerenderPlugin({
+      renderTarget: '#root', // Donde se renderiza la app
+      prerenderScript: path.resolve(__dirname, 'prerender.tsx'),
+      additionalPrerenderRoutes: ['/'], // Pre-renderizar la landing page
+    }),
+  ],
 })
